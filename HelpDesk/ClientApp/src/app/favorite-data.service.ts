c84e9e9bb9b8096b8ea818f562fd39e9e0456d1c
contractcooker <barron.tom@gmail.com>
2020-05-12 14:09:18 -0400

    favorite-data.service.ts

--- HelpDesk/ClientApp/src/app/favorite-data.service.ts
+++ HelpDesk/ClientApp/src/app/favorite-data.service.ts
@@ -1,8 +1,26 @@
 ﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Favorite, JoinedItem } from './interfaces/favorite';
 
 @Injectable()
 export class FavoriteDataService {
-    constructor() {
-
+  userID: number;
+    constructor(private http: HttpClient) {
+      this.userID = Math.floor(Math.random() * 1000000) + 1;
     }
+  getFavorites() {
+    return this.http.get<JoinedItem[]>('/api/cart/' + this.userID);
+  }
+  deleteFavorite(ticketid: number) {
+    return this.http.delete('/api/favorite/' + ticketid);
+  }
+
+  postFavorite(id: number) {
+    let item: Favorite = {
+      ticketid: id,
+      userID: this.userID
+    };
+
+    return this.http.post<CartItem>('/api/favorite', item);
+
 }
