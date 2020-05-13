a36182f15670bb5bb68ac091d1873e73632717e6
Shelton <dellashelton@quickenloans.com>
2020-05-13 11:12:02 -0400

    staging

--- HelpDesk/ClientApp/src/app/tickets/tickets.component.ts
+++ HelpDesk/ClientApp/src/app/tickets/tickets.component.ts
@@ -3,23 +3,36 @@ import { Ticket } from '../interfaces/ticket';
 import { TicketDataService } from '../ticket-data.service';
 
 @Component({
-    selector: 'app-tickets',
-    templateUrl: './tickets.component.html',
-    styleUrls: ['./tickets.component.scss']
+  selector: 'app-tickets',
+  templateUrl: './tickets.component.html',
+  styleUrls: ['./tickets.component.scss']
 })
 /** tickets component*/
 export class TicketsComponent {
   tickets: Ticket[];
+  ticketSection: boolean[] = [];
 
   constructor(private ticketService: TicketDataService) { }
 
-   ngOnInit(): void {
+  ngOnInit(): void {
     this.ticketService.getTickets().subscribe(
       (data: Ticket[]) => {
+
+        for (let i = 0; i < data.length; i++) {
+          this.ticketSection.push(true);
+        }
         this.tickets = data;
       },
       error => console.error(error)
     );
+  }
+
+  showFullTickets(section: number) {
+    for (let i = 0; i < this.ticketSection.length; i++) {
+      this.ticketSection[i] = true;
+    }
+
+    this.ticketSection[section] = false;
   }
 
 }
