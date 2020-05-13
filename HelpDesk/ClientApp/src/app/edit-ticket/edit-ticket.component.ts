18fbb57b31a9b337c24c3864329f674e95ab0c56
Sameer Siddiqui <sameersiddiqui@quickenloans.com>
2020-05-13 12:02:16 -0400

    Pushing all changes before lunch

--- HelpDesk/ClientApp/src/app/edit-ticket/edit-ticket.component.ts
+++ HelpDesk/ClientApp/src/app/edit-ticket/edit-ticket.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { TicketDataService } from '../ticket-data.service';
 import { Ticket } from '../interfaces/ticket';
+import { JoinedItem } from '../interfaces/favorite';
 
 @Component({
   selector: 'app-edit-ticket',
@@ -8,11 +9,30 @@ import { Ticket } from '../interfaces/ticket';
   styleUrls: ['./edit-ticket.component.css']
 })
 export class EditTicketComponent implements OnInit {
-  
+  resolution: string;
+  resolvedby: string;
+  @Input() item: JoinedItem;
+  @Output() updateEmitter = new EventEmitter<Ticket>();
   constructor(private ticketData: TicketDataService) { }
   ticket: Ticket;
 
   ngOnInit() {
+  }
+
+  editTicket(ticketid: number, resolution: string, resolvedby: string) {
+    let ticket: Ticket = {
+      ticketid: 0,
+      userid: this.ticketData.userID,
+      title: this.item.title,
+      category: this.item.category,
+      ticketdetail: this.item.ticketdetail,
+      openedby: this.item.openedby,
+      ticketstatus: true,
+      resolution: resolution,
+      resolvedby: resolvedby
+    }
+
+    this.updateEmitter.emit(ticket);
   }
 
 }
